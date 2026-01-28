579a429968ffc7b8c2f726769d1bbba23ff6ed8a
ssrhaso <hasaana2005@gmail.com>
2026-01-28 20:30:36 +0000

    FEAT: World Model Skeleton

--- src/world_model.py
+++ src/world_model.py
@@ -1,7 +1,14 @@
 """ SKELETON CODE FOR WORLD MODEL MODULE 
 
-- WIP 1 : BASELINE IMPLEMENTATION
+- WIP 1 : BASELINE IMPLEMENTATION - STORM(2023) INSPIRED
 - WIP 2 : TWISTER(2025) / DREAMERv4(2025) INSPIRED IMPROVEMENTS
 
 """
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from dataclasses import dataclass
+from typing import Optional, Tuple, List
+import yaml
+from pathlib import Path
