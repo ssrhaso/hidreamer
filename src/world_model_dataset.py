56a50844b350ff680e0a24bdfc227688aa122e2d
ssrhaso <hasaana2005@gmail.com>
2026-02-11 21:46:51 +0000

    SKELETON: WorldModel Dataset & Data Loader

--- src/world_model_dataset.py
+++ src/world_model_dataset.py
@@ -9,3 +9,20 @@ import torch
 from torch.utils.data import Dataset, DataLoader, random_split
 from typing import List, Tuple
 import yaml
+
+class WorldModelDataset(Dataset):
+    def __init__(self):
+        super().__init__()
+    
+    def __len__(self):
+        pass
+    
+    def __getitem__(self, idx):
+        pass
+    
+
+def create_dataloders():
+    pass
+
+if __name__ == "__main__":
+    pass
