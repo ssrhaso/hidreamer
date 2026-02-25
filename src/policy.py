8d999ad346288a0f915f8c7afe6cd592e28df922
ssrhaso <hasaana2005@gmail.com>
2026-02-25 19:49:56 +0000

    SKELETON: Policy Network

--- src/policy.py
+++ src/policy.py
@@ -11,3 +11,36 @@ import torch.nn.functional as F
 import torch.distributions as D
 from typing import Tuple, Optional
 import math
+
+def symlog():
+    pass
+
+def symexp():
+    pass   
+
+class HierarchicalFeatureExtractor(nn.Module):
+    pass
+
+class PolicyNetwork(nn.Module):
+    pass
+
+class ValueNetwork(nn.Module):
+    pass
+
+class RewardPredictor(nn.Module):
+    pass
+
+class ContinueNetwork(nn.Module):
+    pass
+
+class SlowValueTarget(nn.Module):
+    pass
+
+def compute_lambda_returns():
+    pass
+
+class ReturnNormalizer:
+    pass
+
+def count_policy_params():
+    pass
